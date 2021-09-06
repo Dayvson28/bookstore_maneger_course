e59135801eda5e6e4380d34367d921594f54fe52
dayvson28 <dayvson.medeiros@hotmail.com>
2021-09-06 15:28:09 -0300

    Developed project entities

---
+++ src/main/java/com/dayvsonmedeiros/bookstoremanager/entity/Author.java
@@ -0,0 +1,24 @@
+package com.dayvsonmedeiros.bookstoremanager.entity;
+
+import jdk.jfr.DataAmount;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
